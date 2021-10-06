5a3276714909babd773098320a12acc03b0cf173
hexcowboy <hex@cowboy.dev>
2021-10-06 16:59:08 -0500

    create blockchain to csv export

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { FlowService } from "./FlowService";
 import * as fcl from "@onflow/fcl";
 import * as types from "@onflow/types";
 
-import { clips, moments } from "./shards";
+import { clips, moments, splits } from "./shards";
 
 const Shard = process.env.REACT_APP_FLOW_SHARD_ADDRESS;
 const NonFungibleToken = process.env.REACT_APP_FLOW_NFT_ADDRESS;
 fcl
   .config()
-  .put("accessNode.api", "https://access-testnet.onflow.org")
+  .put("accessNode.api", process.env.REACT_APP_FLOW_ACCESS_NODE)
   .put("0xNonFungibleToken", NonFungibleToken)
   .put("0xShard", Shard);
 
@@ -103,8 +103,8 @@ transaction(influencerID: String, splits: UInt8, metadata: {String: String}) {
 }
         `,
         fcl.args([
-          fcl.arg("Influencer Name", types.String),
-          fcl.arg(3, types.UInt8),
+          fcl.arg(moment.creator, types.String),
+          fcl.arg(splits, types.UInt8),
           fcl.arg(
             [
               { key: "title", value: moment.title },
@@ -123,18 +123,17 @@ transaction(influencerID: String, splits: UInt8, metadata: {String: String}) {
         ...responses,
         { title: `Moment ${moment.title}: `, response: response },
       ]);
-      let sequence = 0;
       for (const clip of clips) {
         if (clip.title === moment.title) {
           const momentId = transaction.events[0].data.id;
-          await createClip(momentId, sequence, clip);
-          sequence++;
+          await createClip(momentId, clip);
         }
       }
     }
   }
 
-  async function createClip(momentId: number, sequence: number, clip: any) {
+  async function createClip(momentId: number, clip: any) {
+    const sequence: number = parseInt(clip.sequence);
     const authorization = service.authz();
     // Create moment
     var response = await fcl.send([
@@ -158,6 +157,7 @@ transaction(momentID: UInt32, sequence: UInt8, metadata: {String: String}) {
           [
             { key: "title", value: clip.title },
             { key: "video_hash", value: clip.video_hash },
+            { key: "video_url", value: clip.video_url },
           ],
           types.Dictionary({ key: types.String, value: types.String })
         ),
@@ -170,7 +170,10 @@ transaction(momentID: UInt32, sequence: UInt8, metadata: {String: String}) {
     const transaction = await fcl.tx(response).onceSealed();
     setResponses((responses) => [
       ...responses,
-      { title: `Clip ${clip.title} ${sequence + 1}/3: `, response: response },
+      {
+        title: `Clip ${clip.title} ${sequence + 1}/3: `,
+        response: response,
+      },
     ]);
     const clipId = transaction.events[0].data.id;
     await batchMint(clipId, 300);
@@ -217,45 +220,104 @@ transaction(recipient: Address, clipID: UInt32, quantity: UInt64) {
 
   async function getShardData() {
     setSending(true);
-    const response = await fcl.send([
+    // ,,,,,,,,,,,
+    // ,id,resourceId,title,description,uri,rarity,edition,editions,image,video_mp4,video_webm
+    // ,${id},${uuid},${title},${description},https://eternal.gg/shards/${id}/,,,,,${video_url},
+    setCsv((csv) => csv + ",,,,,,,,,,,\n");
+    setCsv(
+      (csv) =>
+        csv +
+        ",id,resourceId,title,description,uri,rarity,edition,editions,image,video_mp4,video_webm\n"
+    );
+    const description =
+      "Peer into the shard to see a moment in streaming history. Each Eternal shard is a third of a moment we've immortalized on the blockchain. Collect or pool three shards together to form an Eternal crystal that can be used to enhance a moment in a pack, dropping on October 22. The purer the shard, the higher chance for a rarer moment. All shards and crystals are tradeable at Eternal.gg ";
+    const idRequest = await fcl.send([
       fcl.script`
 import NonFungibleToken from ${NonFungibleToken}
 import Shard from ${Shard}
-pub fun main(): [AnyStruct] {
+pub fun main(): [UInt64] {
     let owner = getAccount(0x${process.env.REACT_APP_FLOW_ACCOUNT_ADDRESS})
         .getCapability(/public/EternalShardCollection)
         .borrow<&{Shard.ShardCollectionPublic}>()
             ?? panic("Could not get receiver reference to the Shard Collection")
-    let allTokens: [AnyStruct] = []
     let ids = owner.getIDs()
-    for id in ids {
-        let tokenData: [AnyStruct] = []
-        let nft = owner.borrowShardNFT(id: id)!
-        let clip = Shard.getClip(clipID: nft.clipID)!
-        let clipMetadata = Shard.getClipMetadata(clipID: clip.id)!
-        let moment = Shard.getMoment(momentID: clip.momentID)!
-        let momentMetadata = Shard.getMomentMetadata(momentID: moment.id)!
-        tokenData.append(id)
-        tokenData.append(owner.borrowNFT(id: id).uuid)
-        for cd in clipMetadata.values {
-            tokenData.append(cd)
-        }
-        for md in momentMetadata.values {
-            tokenData.append(momentMetadata.values)
-        }
-        allTokens.append(tokenData)
-    }
-    return allTokens
+    return ids
 }
       `,
     ]);
-    const allTokens = await fcl.decode(response);
-    console.log(allTokens);
-    console.log(allTokens.length);
-    for (const token in allTokens) {
-      setCsv((csv) => csv + allTokens[token] + "\n");
+    const ids = await fcl.decode(idRequest);
+    console.log(ids);
+    for (const id in ids) {
+      const response = await fcl.send([
+        fcl.script`
+import NonFungibleToken from ${NonFungibleToken}
+import Shard from ${Shard}
+pub struct TokenData{
+    pub let id: UInt64
+    pub let uuid: UInt64
+    pub let clip: Shard.Clip
+    pub let moment: Shard.Moment
+    init(_ id: UInt64, _ uuid: UInt64, _ clip: Shard.Clip, _ moment: Shard.Moment){
+        self.id = id
+        self.uuid = uuid
+        self.clip = clip
+        self.moment = moment
     }
+}
+pub fun main(id: UInt64): TokenData {
+    let owner = getAccount(0x${process.env.REACT_APP_FLOW_ACCOUNT_ADDRESS})
+        .getCapability(/public/EternalShardCollection)
+        .borrow<&{Shard.ShardCollectionPublic}>()
+            ?? panic("Could not get receiver reference to the Shard Collection")
+    let nft = owner.borrowShardNFT(id: id)!
+    let clip = Shard.getClip(clipID: nft.clipID)!
+    let moment = Shard.getMoment(momentID: clip.momentID)!
+    let id = nft.id
+    let uuid = nft.uuid
+    return TokenData(id, nft.uuid, clip, moment)
+}
+      `,
+        fcl.args([fcl.arg(parseInt(id), types.UInt64)]),
+      ]);
 
+      const token = await fcl.decode(response);
+      console.log(token);
+      if (parseInt(id) > 50) return;
+      setCsv(
+        (csv) =>
+          csv +
+          `,${token.id},${token.uuid},${
+            token.moment.metadata.title +
+            `(${parseInt(token.clip.sequence) + 1}/${token.moment.splits})`
+          },${description},https://eternal.gg/shards/${token.id}/,,,,,${
+            token.clip.metadata.video_url
+          },` +
+          "\n"
+      );
+    }
+    setSending(false);
+  }
+
+  async function getNFTCount() {
+    setSending(true);
+    const response = await fcl.send([
+      fcl.script`
+import Shard from ${Shard}
+pub fun main(): Int {
+    let owner = getAccount(0x${process.env.REACT_APP_FLOW_ACCOUNT_ADDRESS})
+        .getCapability(/public/EternalShardCollection)
+        .borrow<&{Shard.ShardCollectionPublic}>()
+            ?? panic("Could not get receiver reference to the Shard Collection")
+    let ids = owner.getIDs()
+    return ids.length
+}
+      `,
+    ]);
+    const amount = await fcl.decode(response);
+    setResponses((responses) => [
+      ...responses,
+      { title: `Total Shard NFTs: ${amount}`, response: response },
+    ]);
     setSending(false);
   }
 
@@ -276,6 +338,9 @@ pub fun main(): [AnyStruct] {
       </button>
       <button onClick={getShardData} disabled={sending}>
         Retrieve all data
+      </button>
+      <button onClick={getNFTCount} disabled={sending}>
+        Get total Shard count
       </button>
       <ul>
         {responses.map((key, index) => (
--- src/shards.ts
+++ src/shards.ts
@@ -1,30 +1,335 @@
+export const splits: number = 3;
 export const moments = [
-  { title: "Throw It Away", game: "Lobanjicaa" },
-  { title: "MrBeast Endgame", game: "PogChamps4" },
+  { game: "Apex", creator: "Rogue", title: "The Pop Off" },
+  { game: "Apex", creator: "Rogue", title: "Humiliation" },
+  { game: "GTA", creator: "JustJamie", title: "Takeoff" },
+  { game: "Halo", creator: "Lucid", title: "Clip It" },
+  { game: "IRL", creator: "IndieFoxx", title: "West Coast" },
+  { game: "IRL", creator: "EXBC", title: "Let's Hit the Karaoke" },
+  { game: "Rust", creator: "Lifestomper", title: "Mini Bombing" },
+  { game: "Rust", creator: "Posty", title: "Posty's DB" },
+  { game: "Resident Evil Village", creator: "IamBrandon", title: "Jumpscare" },
+  { game: "IRL", creator: "Edison 1/1", title: "Will You Marry Me" },
+  { game: "Rocket League", creator: "RL6Mans", title: "Teamplay" },
+  { game: "Chess", creator: "Ludwig", title: "Ludwig Premoves" },
+  { game: "Chess", creator: "MrBeast", title: "MrBeast Beats Ludwig" },
+  { game: "CS:GO", creator: "DaddyLoba", title: "B Is Closed" },
+  { game: "CS:GO", creator: "D0cc", title: "D0cc Takes Mid" },
 ];
 export const clips = [
   {
-    title: "Throw It Away",
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
     video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "The Pop Off",
   },
   {
-    title: "Throw It Away",
-    video_hash: "QmRm3wJkLuVANwt8HJgwqLXCG86R6wuL5LDtRZx3NXjgpE",
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "The Pop Off",
   },
   {
-    title: "Throw It Away",
-    video_hash: "QmcA3ejqBW8iu4xpr3YLeQZo7pVvh2sJTEZ8bzijXcTL9n",
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "The Pop Off",
   },
   {
-    title: "MrBeast Endgame",
-    video_hash: "QmQ3T7G2gdyc3HfKYB5CwezoPPXjsb9eTvxgSic9qc18JP",
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Humiliation",
   },
   {
-    title: "MrBeast Endgame",
-    video_hash: "QmQRPPUx7VQPGVB6AjYVEi4G8qAKoEAmwLMC6mkzqoCa5q",
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Humiliation",
   },
   {
-    title: "MrBeast Endgame",
-    video_hash: "QmWYLMhRP6oA9KnvnUPYfUF9w6f9eVfWzy7XAAyXvJQ4fT",
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Humiliation",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Takeoff",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Takeoff",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Takeoff",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Clip It",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Clip It",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Clip It",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "West Coast",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "West Coast",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "West Coast",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Let's Hit the Karaoke",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Let's Hit the Karaoke",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Let's Hit the Karaoke",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Mini Bombing",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Mini Bombing",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Mini Bombing",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Posty's DB",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Posty's DB",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Posty's DB",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Jumpscare",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Jumpscare",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Jumpscare",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Will You Marry Me",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Will You Marry Me",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Will You Marry Me",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Teamplay",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Teamplay",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Teamplay",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Ludwig Premoves",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Ludwig Premoves",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "Ludwig Premoves",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "MrBeast Beats Ludwig",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "MrBeast Beats Ludwig",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "MrBeast Beats Ludwig",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "B Is Closed",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "B Is Closed",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "B Is Closed",
+  },
+  {
+    sequence: "0",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "D0cc Takes Mid",
+  },
+  {
+    sequence: "1",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "D0cc Takes Mid",
+  },
+  {
+    sequence: "2",
+    video_url:
+      "https://stream.mux.com/4lktyvczZ8sqX5sfdLQa48jpet4b7SF2aiAjtNxDHk4/high.mp4",
+    video_hash: "Qme1SJdaPBKvhjJicXNveepkxe1u6phjiGaQ3p1rEQu6K7",
+    title: "D0cc Takes Mid",
   },
 ];
