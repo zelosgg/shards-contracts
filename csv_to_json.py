92a45f0c0c376090e2cee7e975f8d3b4c35ad5b8
hexcowboy <hex@cowboy.dev>
2021-10-15 08:27:56 -0500

    remove clutter

--- csv_to_json.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('moments.csv') as moments:
-    json = csv.DictReader(moments)
-    print([row for row in json])
-
-with open('clips.csv') as clips:
-    json = csv.DictReader(clips)
-    print([row for row in json])
